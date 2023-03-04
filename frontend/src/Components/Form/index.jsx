3d9efb27e0016186cac45f430c11ef779dee48d8
Patrick Ferreira Carvalho <patrick651265@gmail.com>
2023-03-04 01:03:26 -0300

    Production Debbuging

--- frontend/src/Components/Form/index.jsx
+++ frontend/src/Components/Form/index.jsx
@@ -11,6 +11,7 @@ export const Form = () => {
     const [team, setTeam] = useState(0);
     
     const fetchData = async () => {
+        console.log(import.meta.env.BACKEND_URL);
         await fetch(`${import.meta.env.BACKEND_URL}/api/teams`)
             .then(async res => await res.json())
             .then(teams => setItems(teams.map(team => ({ id: team.id, name: team.nome }))))
